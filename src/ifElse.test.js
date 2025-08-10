e3068e01e61d6938966c3558d7d8a95af2322530
Vlad <vladkygot@gmail.com>
2025-08-10 16:33:35 +0300

    add task solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -7,28 +7,28 @@ describe('ifElse', () => {
     expect(ifElse).toBeInstanceOf(Function);
   });
 
-  it('should call first callback if condition returns true', () => {
+  it('should call first callback with no arguments if condition returns true', () => {
     const condition = jest.fn(() => true);
     const first = jest.fn();
     const second = jest.fn();
 
     const result = ifElse(condition, first, second);
 
-    expect(condition).toHaveBeenCalledTimes(1);
-    expect(first).toHaveBeenCalledTimes(1);
+    expect(condition).toHaveBeenCalledWith();
+    expect(first).toHaveBeenCalledWith();
     expect(second).not.toHaveBeenCalled();
     expect(result).toBeUndefined();
   });
 
-  it('should call second callback if condition returns false', () => {
+  it('should call second callback with no arguments if condition returns false', () => {
     const condition = jest.fn(() => false);
     const first = jest.fn();
     const second = jest.fn();
 
     const result = ifElse(condition, first, second);
 
-    expect(condition).toHaveBeenCalledTimes(1);
-    expect(second).toHaveBeenCalledTimes(1);
+    expect(condition).toHaveBeenCalledWith();
+    expect(second).toHaveBeenCalledWith();
     expect(first).not.toHaveBeenCalled();
     expect(result).toBeUndefined();
   });
